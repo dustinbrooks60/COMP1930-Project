4c2fedfc4328603efc6aecdc071fcae2936380f6
Dustin Brooks <dustinbrooks60@gmail.com>
2019-03-18 11:30:52 -0700

    Added login page and firebase script

---
+++ setup_firebase.js
@@ -0,0 +1,14 @@
+(function(){  
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCRNyjKsPc9-5T0D3NHXiqDnqU8x93XwSk",
+    authDomain: "sharegarden-d68ce.firebaseapp.com",
+    databaseURL: "https://sharegarden-d68ce.firebaseio.com",
+    projectId: "sharegarden-d68ce",
+    storageBucket: "",
+    messagingSenderId: "337652476919"	
+  };
+  firebase.initializeApp(config);
+  app_firebase = firebase; 
+})();
